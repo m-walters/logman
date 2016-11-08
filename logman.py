e3eb9755e50fb95277637459e11ed892f0cadb10
m-walters <walters.m93@gmail.com>
2016-11-08 09:39:54 -0500

    minor change

--- logman.py
+++ logman.py
@@ -1,33 +1,32 @@
 import logging
 
 class logman:
-    def __init__(self, name, level, logfile, sformat):
-        level = level.upper() 
-        self.name = name 
-        self.logger = logging.getLogger(name)
-        self.logger.setLevel(logging.DEBUG)
-        handle = logging.FileHandler(logfile)
-        handle.setLevel(getattr(logging, level))
-        handle.setFormatter(logging.Formatter(sformat))
-        self.logger.addHandler(handle)
+    def __init__(self, name, level, sformat, logfile=None):
+	level = level.upper()
+	self.name = name
+	self.logger = logging.getLogger(name)
+	self.logger.setLevel(logging.DEBUG)
+	if logfile:
+	    handle = logging.FileHandler(logfile)
+	else:
+	    handle = logging.StreamHandler()
+	handle.setLevel(getattr(logging, level))
+	handle.setFormatter(logging.Formatter(sformat))
+	self.logger.addHandler(handle)
 
 
     def add_handler(self, level, sformat, fname=None):
-        level = level.upper() 
-        logging.getLogger(self.name) 
-        if not fname:
-            ch = logging.StreamHandler()
-            ch.setLevel(getattr(logging, level))
-            ch.setFormatter(logging.Formatter(sformat))
-            self.logger.addHandler(ch)
-        else:
-            fh = logging.FileHandler(fname)
-            fh.setLevel(getattr(logging, level))
-            fh.setFormatter(logging.Formatter(sformat))
-            self.logger.addHandler(fh) 
+        level = level.upper()
+	logging.getLogger(self.name)
+        if fname:
+	    handle = logging.FileHandler(fname)
+	else:
+	    handle = logging.StreamHandler()
+	handle.setLevel(getattr(logging, level))
+	handle.setFormatter(logging.Formatter(sformat))
+	self.logger.addHandler(handle)
 
 
     def printl(self, level, msg):
-        level = level.lower() 
+	level = level.lower()
         getattr(self.logger, level)(msg)
-
